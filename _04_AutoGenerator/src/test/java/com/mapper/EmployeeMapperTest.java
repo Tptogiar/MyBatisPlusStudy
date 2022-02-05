ac94f07584370d123c990ccb9b58783ef8b368f5
Tptogiar <2528891112@qq.com>
2022-02-06 00:06:44 +0800

    test

---
+++ _04_AutoGenerator/src/test/java/com/mapper/EmployeeMapperTest.java
@@ -0,0 +1,11 @@
+package com.mapper;
+
+import junit.framework.TestCase;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmployeeMapperTest extends TestCase {
+
+    public void testName() {
+
+    }
+}
